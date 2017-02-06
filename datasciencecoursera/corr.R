2b1f4916dd81f9cab0e9adb5bc96de445228d325
aedobbyn <adobbs11@gmail.com>
2017-02-05 23:41:53 -0600

    getting there with corr

--- datasciencecoursera/corr.R
+++ datasciencecoursera/corr.R
@@ -14,21 +14,23 @@ get_zeros <- function(file_num) {
 
 
 # Calculate the mean of the pollutant column for each file
-corr <- function(directory, threshold = 0) {
+corr <- function(directory, threshold = 0, id = 1:332) {
   out <- vector()
   for (i in seq_along(id)) {
     file <- readr::read_csv(paste0(directory, "/", get_zeros(i), i, ".csv"))
-    these_cases <- sum(complete.cases(file) == TRUE) {
-      if (these_cases >= threshold)
-        this_cor <- cor(file[[sulfate]], file[[nitrate]])
-        out[[i]] <- rbind(out, this_cor)
-    } else {
-      out <- 0
-    }
-  out
+    complete_cases <- sum(complete.cases(file) == TRUE) 
+      if (complete_cases > threshold) {
+        this_cor <- cor(x = file[["sulfate"]], y = file[["nitrate"]], use = "complete.obs")
+        out <- rbind(out, this_cor)
+        print(paste0("out = ", out))
+      } else {
+        out <- 0
+      }
+    out
   }
 }
 
-cr <- corr(directory = "./specdata", threshold = 50)
+
+cr <- corr(directory = "./specdata", threshold = 50, id = 1:3)
 cr
 
