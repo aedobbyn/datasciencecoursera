b5f1cb8fb6b37c29826ba5a9917feff0b1262c38
aedobbyn <adobbs11@gmail.com>
2017-02-06 00:21:33 -0600

    okay going to try a different tack to get a vector 0 out

--- datasciencecoursera/corr.R
+++ datasciencecoursera/corr.R
@@ -14,24 +14,28 @@ get_zeros <- function(file_num) {
 
 
 # Calculate the mean of the pollutant column for each file
-corr <- function(directory, threshold = 0) {
-  id <- 1:332
+corr <- function(directory, threshold = 0, id = 1:2) {
+  # id <- 1:332
   out <- vector()
   for (i in seq_along(id)) {
     file <- readr::read_csv(paste0(directory, "/", get_zeros(i), i, ".csv"))
     complete_cases <- sum(complete.cases(file) == TRUE) 
       if (complete_cases > threshold) {
         this_cor <- cor(x = file[["sulfate"]], y = file[["nitrate"]], use = "complete.obs")
-      } else {
-        this_cor <- 0
-      }
-    out <- rbind(out, this_cor)
+        out <- rbind(out, this_cor)
+      } 
+  } 
+  if (is.null(out)) {
+    print(class(out))
+    out <- as.numeric(out)
   }
-  as.vector(out)
+  return(as.vector(out))
+  print(paste0("this is", out))
+  # out
 }
 
 
-cr <- corr(directory = "./specdata", threshold = 150)
+cr <- corr(directory = "./specdata", threshold = 10000, id = 1:2)
 head(cr)
 
 
