9c52d261c3ed3502a5fbd3ebf326291806e63cc7
aedobbyn <adobbs11@gmail.com>
2017-02-06 00:25:23 -0600

    hack worked

--- datasciencecoursera/corr.R
+++ datasciencecoursera/corr.R
@@ -14,9 +14,9 @@ get_zeros <- function(file_num) {
 
 
 # Calculate the mean of the pollutant column for each file
-corr <- function(directory, threshold = 0, id = 1:2) {
-  # id <- 1:332
-  out <- vector()
+corr <- function(directory, threshold = 0) {
+  id <- 1:331
+  out <- 0
   for (i in seq_along(id)) {
     file <- readr::read_csv(paste0(directory, "/", get_zeros(i), i, ".csv"))
     complete_cases <- sum(complete.cases(file) == TRUE) 
@@ -25,17 +25,14 @@ corr <- function(directory, threshold = 0, id = 1:2) {
         out <- rbind(out, this_cor)
       } 
   } 
-  if (is.null(out)) {
-    print(class(out))
-    out <- as.numeric(out)
+  if (length(out) > 1) {
+    out <- out[2:length(out)]
   }
   return(as.vector(out))
-  print(paste0("this is", out))
-  # out
 }
 
 
-cr <- corr(directory = "./specdata", threshold = 10000, id = 1:2)
+cr <- corr(directory = "./specdata", threshold = 10)
 head(cr)
 
 
